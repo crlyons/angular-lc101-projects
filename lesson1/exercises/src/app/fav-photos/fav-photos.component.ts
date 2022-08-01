55bbab629f8ca832d342193d4355459cf6538f20
Carl <cindymadewhat@gmail.com>
2022-08-01 16:02:22 -0500

    idk man

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Very Cool Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://theconversation.com/physics-and-psychology-of-cats-an-improbable-conversation-176020';
+  image3 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 
   constructor() { }
 
